e3702d65f2edcb603e46551d5578d5bce78e1d57
anilikarikatti <anilkarikatti333@gmail.com>
2023-04-01 13:45:47 +0530

    template1 is ready

--- pages/components/grid.js
+++ pages/components/grid.js
@@ -5,7 +5,7 @@ export default function Grid (){
         <div style={{height:"100vh"}}>
             <div style={{display:"grid",gridTemplateColumns: "1fr 1fr 1fr",width:"100px"}}>
                 {arr.map((elem)=>{
-                    return  <div style={{border:"2px solid black " ,height:"20px"}}>{elem}</div>})}
+                    return  <div style={{border:"2px solid black " ,height:"20px"}} key={elem}>{elem}</div>})}
                 {/* <div style={{border:"2px solid black " ,height:"20px"}}></div>
                 <div style={{border:"2px solid black " ,height:"20px"}}></div>
                 <div style={{border:"2px solid black " ,height:"20px"}}></div>
