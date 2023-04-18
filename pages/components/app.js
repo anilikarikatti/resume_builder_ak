21ac0bccf5aef547a6cd3f07c13f51b9b23ee6e2
anilikarikatti <anilkarikatti333@gmail.com>
2023-04-18 11:01:54 +0530

    main page added

---
+++ pages/components/app.js
@@ -0,0 +1,18 @@
+
+import Link from "next/link"
+
+export default function MainPage(){
+
+
+    return(
+        <>
+            <div className="h-screen">
+                <div className="bg-red-300">
+                    
+                </div>
+
+                <div><Link href="/components/input.js">click</Link></div>
+            </div>
+        </>
+    )
+}
